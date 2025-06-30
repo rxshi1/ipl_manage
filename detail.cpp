64c814453bf4bff7068a4915e10b21ca5ee69aa9
Rishi <rishitalesara7224@gmail.com>
2025-06-30 13:37:58 +0530

    first commit

---
+++ detail.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Book
+{
+    public:
+  string book_title;
+  int id;
+  string author;
+  Book* head = NULL;
+  Book *next;
+};
+
+class issuedBook
+{
+   public:
+  string title;
+  int id;
+  issuedBook* rear = NULL;
+  issuedBook* front = NULL;
+  issuedBook* next;
+};
+
+void addBook()
+{
+    Book* newBook = new Book();
+    cout << " please enter the book ID : " ; 
+    cin >> newBook->id;
+    
+}
